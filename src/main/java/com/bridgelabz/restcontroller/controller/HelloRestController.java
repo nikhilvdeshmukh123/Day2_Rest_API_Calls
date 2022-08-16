6fc48e99197dde14a563110a1fda00805c80c158
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-08-17 01:06:59 +0530

    Added: PostRequest pass first name and last name in body

--- src/main/java/com/bridgelabz/restcontroller/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/restcontroller/controller/HelloRestController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.restcontroller.controller;
 
+import com.bridgelabz.restcontroller.entity.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,18 +8,22 @@ import org.springframework.web.bind.annotation.*;
 public class HelloRestController {
 
     @GetMapping(value = {"", "/", "/home"})
-    public String Hello(){
+    public String Hello() {
         return "Hello from Bridgelabz";
     }
 
     @GetMapping(value = {"/query"})
-    public String sayHello(@RequestParam String name){
+    public String sayHello(@RequestParam String name) {
         return " Hello " + name + "!";
     }
 
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name) {
-        return " Hello " + name + "!" ;
+        return " Hello " + name + "!";
     }
 
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello" + user.getFirstName() + "" + user.getLastName() + "!";
+    }
 }
